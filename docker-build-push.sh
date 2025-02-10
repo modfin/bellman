c0ecd73836bb55f72ab8ab8789b2f346079ba618
Rasmus Holm <rasmus.holm@modularfinance.se>
2025-02-10 14:15:57 +0100

    added sematic version

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+semantic=$(git describe | cut -f1 -d'-')
 version=$(git describe | cut -f1,2 -d'-')
 
 BUILDER=$(docker buildx create) || exit 1
@@ -9,6 +10,7 @@ docker buildx build \
   --platform linux/amd64,linux/arm64 \
   -f ./Dockerfile \
   -t modfin/bellman:latest \
+  -t modfin/bellman:"${semantic}" \
   -t modfin/bellman:"${version}" \
   --push \
   .
