5c6f9e5b4c6937b223be161e6461be2ece63c21b
Alexander Olsson <alexander.olsson@modularfinance.se>
2026-01-26 17:47:25 +0100

    Added correct think parameter on ollama

--- services/ollama/request.go
+++ services/ollama/request.go
@@ -29,6 +29,7 @@ type genRequest struct {
 	Format *JSONSchema `json:"format,omitempty"`
 
 	Option genRequestOption `json:"option,omitempty"`
+	Think  bool             `json:"think"`
 	Stream bool             `json:"stream"`
 
 	Tools []tool `json:"tools,omitempty"`
