6ebeef3f05ea1e754f49666c873a3d60abf8813b
agaton <agaton.sjoberg@modularfinance.se>
2026-04-24 10:59:06 +0200

    adding xai integration tests

---
+++ services/xai/xai_integration_test.go
@@ -0,0 +1,29 @@
+package xai_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/modfin/bellman/models/gen"
+	"github.com/modfin/bellman/services/xai"
+	"github.com/modfin/bellman/testsuite"
+)
+
+func TestXAIIntegration(t *testing.T) {
+	key := os.Getenv("XAI_API_KEY")
+	if key == "" {
+		t.Skip("XAI_API_KEY not set")
+	}
+
+	client := xai.New(key)
+	g := client.Generator(gen.WithModel(xai.GenModel_grok_4_1_fast_reasoning))
+
+	testsuite.Run(t, g, testsuite.Capabilities{
+		Tools:               true,
+		StructuredOutput:    true,
+		Streaming:           true,
+		Agent:               true,
+		StreamThinkingTools: true,
+	})
+
+}
